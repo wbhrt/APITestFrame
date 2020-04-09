e3c47bae0d03d9800e194f60f145dc39a6ddc733
1426858895@qq.com <1426858895@qq.com>
2020-04-09 11:47:25 +0800

    归并排序和快速排序

--- util/common_method.py
+++ util/common_method.py
@@ -46,8 +46,66 @@ def is_equal_dic(dic_one,dic_two):
     if isinstance(dic_two, str):
         dic_two = json.loads(dic_two)
     cmp(dic_one,dic_two)
+
+#快速排序
+def min_location(list,low,high):
+    i = low-1
+    num = list[high]
+    for j in range(low,high):
+        if list[j]<=num:
+            i = i+1
+            list[i],list[j]= list[j],list[i]
+    list[i+1],list[high] = list[high],list[i+1]
+    return i+1
+
+def fastsort(list,low,high):
+    if low < high:
+        pi = min_location(list,low,high)
+        fastsort(list,low,pi-1)
+        fastsort(list, pi+1, high)
+ #快速排序
+def quicsort(list):
+    if len(list) <2:
+        return list
+    mid = list[len(list) // 2]
+    low,high=[],[]
+    list.remove(mid)
+    for i in list:
+        if i >=mid:
+            high.append(i)
+        else:
+            low.append(i)
+    return quicsort(low)+[mid]+quicsort(high)
+#归并排序
+def merge(left,right):
+    arr = []
+    i=j=0
+    while i <len(left) and j <len(right):
+        if left[i] <right[j]:
+            arr.append(left[i])
+            i +=1
+        else:
+            arr.append(right[j])
+            j +=1
+    if i == len(left):
+        for n in right[j:]:
+            arr.append(n)
+    else:
+        for m in left[i:]:
+            arr.append(m)
+    return arr
+def mergesort(list):
+    if len(list) <=1:
+        return list
+    mid = len(list)//2
+    left = mergesort(list[:mid])
+    right = mergesort(list[mid:])
+    return merge(left,right)
 if __name__ == '__main__':
     list = [1,2,7,8,9,4]
-    num = func(list)
-    print(num)
+    print(mergesort(list))
+    # fastsort(list,0,5)
+    # for i in list:
+    #     print(i)
+
 
